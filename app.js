a049dc407ac5750852447c7fa4cd41dcfd36e90a
Your Name <you@example.com>
2018-10-09 14:03:40 +0200

    gyakorlat vegi allapot

---
+++ app.js
@@ -0,0 +1,39 @@
+// 1. lépés: parancssorba npm init
+// 2. lépés: npm install express body-parser --save
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// létrehozok egy expressjs szerveralkalmazást
+var app = express();
+
+app.use(bodyParser.urlencoded(
+    {'extended': true}));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+//definiálom a route-okat
+router.get('/about', function (req, res) {
+  res.send('Ez egy minta')
+});
+
+router.post('/about', function(req, res) {
+    console.log("Hello a nevem " + req.body.username 
+    + " " + req.body.age + " éves vagyok és " + 
+    req.body.foglalkozas);
+    if(req.body.foglalkozas) {
+        return res.status(200).send("Köszönöm az infót");
+    }
+    return res.status(404).send("Ne aggodj valojaban minden oké");
+});
+
+//élesítem a route-okat
+app.use('/', router);
+
+app.listen(3500, function() {
+    console.log('A szerver fut port 3500-on');
+});
+
+// 3. lépés: node app.js  
+// 4. lépés: a leállítása a Ctrl+C-vel történik   
